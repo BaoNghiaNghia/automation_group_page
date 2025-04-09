4459e8e4ee3341f2cb59eba885bfb745550a716b
BaoNghiaNghia <baonghia.kht@gmail.com>
2025-04-09 21:39:25 +0700

    update code

--- backend/constants.py
+++ backend/constants.py
@@ -2,7 +2,11 @@ API_KEY_CAPTCHA = 'point_3d0bd505d511c336b6279f4815057b9a'
 FB_DEFAULT_URL = "https://www.facebook.com"
 FB_ACCOUNT_LIST = [
     ("0399988593", "p6+p7N&r%M$#B5b"),
-    ("Cdy006947@gmail.com", "Haha123@")
+    ("Cdy006947@gmail.com", "Haha123@"),
+    
+    # ("0928649691", "vutuan1985@"),
+    # ("0921747844", "vutuan1985@"),
+    # ("0928618253", "vutuan1985@"),
 ]
 GAME_NAME_URL = "OnceHumanOfficial"
 SERVICE_URL = "https://boostgamemobile.com/service"
@@ -17,3 +21,6 @@ GEMINI_MODEL = "gemini-2.0-flash"
 
 DEEPSEEK_API_KEY = "sk-e71d03c8c9a44344b3c39ef6db11526c"
 DEEPSEEK_MODEL = "deepseek-chat"
+
+# adb -s emulator-5554 shell input tap 456.9 168.5
+# adb -s emulator-5554 shell input text "Danh Tướng Origin ✅ Group Chính Thức"
