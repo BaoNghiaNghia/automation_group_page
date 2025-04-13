a662220951909defc10d2dfaf20298fa0063b666
BaoNghiaNghia <baonghia.kht@gmail.com>
2025-04-13 13:14:08 +0700

    update code

--- backend/constants.py
+++ backend/constants.py
@@ -9,8 +9,8 @@ FB_ACCOUNT_LIST = [
     # ("0928618253", "vutuan1985@"),
 ]
 GAME_NAME_URL = "OnceHumanOfficial"
-# SERVICE_URL = "https://boostgamemobile.com/service"
-SERVICE_URL = "http://127.0.0.1:8080/service"
+SERVICE_URL = "https://boostgamemobile.com/service"
+# SERVICE_URL = "http://127.0.0.1:8080/service"
 
 DOMAIN_CAPTCHA = "https://captcha69.com"
 FOLDER_PATH_DATA_CRAWLER = "/data_crawler/"
