7dfa0fbcb68bcf169a415a2d8322d9473efc1898
BaoNghiaNghia <baonghia.kht@gmail.com>
2025-07-17 18:58:55 +0700

    update code

--- clone_instances_step_1.bat
+++ clone_instances_step_1.bat
@@ -1,13 +1,21 @@
 @echo off
 setlocal
-set TEMPLATE=D:\LDPlayer\LDPlayer9\vms\leidian0
-set TARGET=D:\LDPlayer\LDPlayer9\vms
 
-for /L %%i in (0,1,340) do (
+set TEMPLATE_FOLDER=D:\LDPlayer\LDPlayer9\vms\leidian0
+set TARGET_FOLDER=D:\LDPlayer\LDPlayer9\vms
+set CONFIG_FOLDER=D:\LDPlayer\LDPlayer9\vms\config
+
+for /L %%i in (1,1,340) do (
     echo ========================
     echo Tạo instance leidian%%i
-    xcopy "%TEMPLATE%" "%TARGET%\leidian%%i" /E /Y /I
-    timeout /t 1 /nobreak >nul
+
+    rem Copy thư mục VM template
+    xcopy "%TEMPLATE_FOLDER%" "%TARGET_FOLDER%\leidian%%i" /E /Y /I
+
+    rem Copy file cấu hình .config
+    copy /Y "%CONFIG_FOLDER%\leidian0.config" "%CONFIG_FOLDER%\leidian%%i.config"
+
+    timeout /t 7 /nobreak >nul
 )
 
 pause
