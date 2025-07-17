bd04f6b49838af2a3437eb0d3e9d61809e7cb436
BaoNghiaNghia <baonghia.kht@gmail.com>
2025-07-17 18:52:05 +0700

    update code

--- clone_instances_step_1.bat
+++ clone_instances_step_1.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal
 set TEMPLATE=D:\LDPlayer\LDPlayer9\vms\leidian0
 set TARGET=D:\LDPlayer\LDPlayer9\vms
 
@@ -6,5 +7,7 @@ for /L %%i in (0,1,340) do (
     echo ========================
     echo Tạo instance leidian%%i
     xcopy "%TEMPLATE%" "%TARGET%\leidian%%i" /E /Y /I
+    timeout /t 1 /nobreak >nul
 )
+
 pause
