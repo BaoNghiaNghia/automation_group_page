db0c69d5b47b75569d32cd45df90945ec29f04a3
BaoNghiaNghia <baonghia.kht@gmail.com>
2026-02-16 16:21:48 +0700

    update code

--- sync_post_into_database.py
+++ sync_post_into_database.py
@@ -14,16 +14,16 @@ if __name__ == "__main__":
     logger.info(f"Running in {args.environment} environment")
     
     try:
-        for i in range(30):
-            logger.info(f"===== Run {i+1}/30 =====")
+        for i in range(100):
+            logger.info(f"===== Run {i+1}/100 =====")
             sync_post_into_database(args.environment)
             logger.info("Sync completed successfully.")
 
-            if i < 29:  # Không sleep sau lần cuối
+            if i < 99:  # Không sleep sau lần cuối
                 logger.info("Waiting 10 seconds before next run...")
                 time.sleep(10)
 
-        logger.info("✅ All 30 sync cycles completed successfully.")
+        logger.info("✅ All 100 sync cycles completed successfully.")
 
     except Exception as e:
         logger.error(f"Unexpected error during run: {str(e)}", exc_info=True)
